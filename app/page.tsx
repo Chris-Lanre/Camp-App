7ff79748f106835da479fd15b4129a028e93b538
Chris-Lanre <121674000+Chris-Lanre@users.noreply.github.com>
2023-11-15 03:56:40 -0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Camp from "./components/Camp";
+import Features from "./components/Features";
+import GetApp from "./components/GetApp";
+import Guide from "./components/Guide";
+import Heroo from "./components/Heroo";
+
+export default function Home() {
+  return (
+   <>
+    <Heroo />
+    <Camp />
+    <Guide />
+    <Features />
+    <GetApp />
+
+
+   </>
+  )
+}
